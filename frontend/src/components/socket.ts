f0cc5911880d1137f59daece4e255983bc0b4810
Shikhar Shetty <shettyshikhar99@gmail.com>
2025-12-23 12:23:57 +0530

    fix: url fixes and secrets setup for deployment

--- frontend/src/components/socket.ts
+++ frontend/src/components/socket.ts
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://opsentrix.onrender.com", {
+export const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
     autoConnect: false,
     transports: ["websocket", "polling"],
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000
 })
+
